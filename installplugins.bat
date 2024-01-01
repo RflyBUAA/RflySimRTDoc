96189f6cc971a9263c8a3cb3794c1389d6c65dc2
kcx064 <kcx064@163.com>
2024-01-01 15:25:15 +0800

    新增windows下一键配置脚本

---
+++ installplugins.bat
@@ -0,0 +1,20 @@
+pip install --upgrade pip
+python -m pip install mkdocs
+python -m mkdocs
+pip install mkdocs-pdf-export-plugin
+pip install pymdown-extensions
+pip install markdown-callouts
+pip install mdx-gh-links
+pip install mkdocs-click
+pip install mkdocs-autorefs
+pip install mkdocstrings
+pip install 'mkdocstrings[crystal,python]'
+pip install mkdocs-gitbook
+pip install mkdocs-with-pdf
+pip install markdown-checklist
+pip install mkdocs-video
+pip install mkdocs-git-revision-date-localized-plugin
+pip install markdown-captions
+pip install mkdocs-resize-images
+pip install mkdocs-git-latest-changes-plugin
+pip install mkdocs-latest-release-plugin
